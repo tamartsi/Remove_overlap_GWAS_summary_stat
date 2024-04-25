b4ac8757ddb8898394f9dc373319a399d77da5ff
Tamar Sofer <tamartsi@users.noreply.github.com>
2024-04-25 13:02:55 -0400

    added more sophisticated example
    Added an example of meta-analysis of 4 studies, and looking whether removing one of them is like meta-analyzing the other three.

--- test_remove_overlap_gwas.R
+++ test_remove_overlap_gwas.R
@@ -24,3 +24,40 @@ res <- remove_overlap_gwas(beta_all, se_all, beta1, se1)
 
 all(abs(res$beta_keep - beta2) < 1e-10) # TRUE
 all(abs(res$se_keep - sqrt(v2)) < 1e-10) # TRUE
+
+
+## more sophisticated example: meta-analyze summary statistics from multiple 
+## studies, then assess the "removal" of one of them. 
+require(metafor)
+asn <- read.csv("asn.csv")
+eur <- read.csv("eur.csv")
+afr <- read.csv("afr.csv")
+his <- read.csv("his.csv")
+
+res <- data.frame(SNP = asn$SNP, Est_all = NA, SE_all = NA, Est_asn_eur_afr = NA, SE_asn_eur_afr = NA)
+
+for (i in 1:nrow(res)){
+  cur_res <- rma(yi = c(asn$Est[i], eur$Est[i], afr$Est[i], his$Est[i]), 
+             vi = c(asn$SE[i]^2, eur$SE[i]^2, afr$SE[i]^2, his$SE[i]^2), 
+             method="FE")
+  
+  res[i, "Est_all"] <- cur_res$beta
+  res[i, "SE_all"] <- cur_res$se
+  
+  cur_res <- rma(yi = c(asn$Est[i], eur$Est[i], afr$Est[i]), 
+                 vi = c(asn$SE[i]^2, eur$SE[i]^2, afr$SE[i]^2), 
+                 method="FE")
+  
+  res[i, "Est_asn_eur_afr"] <- cur_res$beta
+  res[i, "SE_asn_eur_afr"] <- cur_res$se
+  
+}
+
+
+# now add an estimate from the remove overlap function:
+res_rm_his <- remove_overlap_gwas(res$Est_all, res$SE_all, his$Est, his$SE)
+
+
+all(abs(res_rm_his$beta_keep - res$Est_asn_eur_afr) < 1e-10) # TRUE
+all(abs(res_rm_his$se_keep - res$SE_asn_eur_afr) < 1e-10) # TRUE
+
